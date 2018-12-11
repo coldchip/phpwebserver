1d61732c1f1149bf70ac7aaeaca4404d65b768a3
coldchip <growwick@gmail.com>
2018-12-11 16:51:11 +0800

    Add files via upload

---
+++ example/httpserver.php
@@ -0,0 +1,24 @@
+<?php
+
+require("../src/server.php");
+
+// Make sure you turn off other web servers
+
+$server = new WebServer("0.0.0.0", 80);
+$server->createHTTPServer();
+
+$server->on("/", function($server, $client){
+    $server->send($client, "<h1>Hello</h1>", 404);
+});
+
+$server->on("/test", function($server, $client){
+    $server->sendFile($client, "/var/www/test.jpg");
+});
+
+$server->on("*", function($server, $client){
+    $server->redirect($client, "/test");
+});
+
+$server->run();
+
+?>
